a75e9557d9256e7b5b431b8e49d49794da9b7709
Kyle Marko <kyle.marko@csuglobal.edu>
2026-03-25 16:42:39 -0400

    First commit

--- src/App.java
+++ src/App.java
@@ -1,3 +1,45 @@
+// Your Portfolio Project for CSC320 will consist of three components:
+
+// Program corrections: Make the appropriate corrections to all the programming assignments submitted as Critical Thinking assignments from Modules 1-6. You will need to submit the programs along with the carefully outlined corrections needed in order for the programs to run correctly.
+// Lessons learned reflection: Create a 2-3-page summary that outlines the lessons learned in this Programming I course.
+// Final program: Create a final program that meets the requirements outlined below.
+// Final Program Requirements
+// Create an automobile class that will be used by a dealership as a vehicle inventory program. The following attributes should be present in your automobile class:
+
+// private string make
+// private string model
+// private string color
+// private int year
+// private int mileage
+// Your program should have appropriate methods such as:
+
+// default constructor
+// parameterized constructor
+// add a new vehicle  method
+// list vehicle information (return string array)
+// remove a vehicle method
+// update vehicle attributes method
+// All methods should include try..catch constructs. Except as noted all methods should return a success or failure message (failure message defined in "catch").
+
+// Create an additional class to call your automobile class (e.g., Main or AutomobileInventory). Include a try..catch construct and print it to the console.
+// Call automobile class with parameterized constructor (e.g., "make, model, color, year, mileage").
+// Then call the method to list the values. Loop through the array and print to the screen.
+// Call the remove vehicle method to clear the variables.
+// Print the return value.
+// Add a new vehicle.
+// Print the return value.
+// Call the list method and print the new vehicle information to the screen.
+// Update the vehicle.
+// Print the return value.
+// Call the listing method and print the information to the screen.
+// Display a message asking if the user wants to print the information to a file (Y or N).
+// Use a scanner to capture the response. If "Y", print the file to a predefined location (e.g., C:\Temp\Autos.txt). Note: you may want to create a method to print the information in the main class.
+// If "N", indicate that a file will not be printed.
+// Your final program submission materials must include your source code and screenshots of the application executing the application and the results.
+
+// Compile your Module 1-6 programs with corrections, lessons learned reflection, and final program course code and application screenshots into a single document. Submit your completed Portfolio Project by the posted due date.
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ src/moduleSixCheckIn.java
@@ -0,0 +1,21 @@
+// Submit document with methods for your automobile class, and pseudo code indicating functionality of each method.
+
+// Example:
+
+// public String RemoveVehicle(String
+
+// autoMake, String autoModel, String autoColor, int autoYear)
+
+// If
+
+//              values entered match values stored in private variables
+
+//              remove vehicle information
+
+// else
+
+//              return message indicating mismatch
+
+// public class moduleSixCheckIn {
+    
+// }
